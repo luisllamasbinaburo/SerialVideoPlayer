7f0385b43a800e1f6d53f27cf85eb06d83ca4012
luisllamasbinaburo <luisllamas@hotmail.com>
2019-01-11 13:59:28 +0100

    Change name to SerialVideoPlayer

--- SimpleVideoPlayer/Properties/AssemblyInfo.cs
+++ SimpleVideoPlayer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleVideoPlayer")]
+[assembly: AssemblyTitle("SerialVideoPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleVideoPlayer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("www.luisllamas.es")]
+[assembly: AssemblyProduct("SerialVideoPlayer")]
+[assembly: AssemblyCopyright("Copyright ©2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
