932489e3028843c43b465cbbe30a39b06f8ef719
Luis Llamas <luisllamas@hotmail.com>
2019-01-11 16:51:21 +0100

    Added ArduinoDemo

---
+++ ArduinoDemo.ino
@@ -0,0 +1,17 @@
+void setup() 
+{
+	Serial.begin(9600);
+	delay(10000);
+}
+
+void loop()
+{
+	Serial.println("A");
+	delay(5000);
+	Serial.println("B");
+	delay(5000);
+	Serial.println("VIDEO1");
+	delay(5000);
+	Serial.println("VIDEO2");
+	delay(5000);
+}
